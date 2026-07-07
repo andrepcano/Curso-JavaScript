8fb2e5b3a8731c6d36fac597b912edc181f665fb
Andre Cano <andrepcano@gmail.com>
2026-07-07 20:24:46 -0300

    feat: fazendo a aula 36

---
+++ aula36/index.js
@@ -0,0 +1,35 @@
+// For in -> le os indices ou chaves do objeto
+const frutas = ['Pera', 'Maca', 'Uva'];
+const pessoa = {
+    nome: "André",
+    sobrenome: "Peixoto Cano",
+    idade: 18
+};
+
+// vem o nome e a resposta
+// for (let chave in pessoa) {
+//     console.log(chave, pessoa[chave]);
+// }
+
+// console.log(pessoa.nome);
+// console.log(pessoa['nome']);
+
+// pega as chaves
+// for (let i in pessoa) {
+//     console.log(i)
+// }
+
+// for (let indice in frutas) {
+//     console.log(frutas[indice]);
+// }
+
+// lendo os indices do array
+// for ( let i in frutas) {
+//     console.log(i);
+// }
+
+// for(let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
+
+
